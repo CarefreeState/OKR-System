a603bd978a423403acca142c6b44b7aad1887ebe
s:103 <mamingsheng103@yeah.net>
2024-02-17 19:03:05 +0800

    调整

--- user-service/src/main/java/com/macaku/user/interceptor/config/VisitConfig.java
+++ user-service/src/main/java/com/macaku/user/interceptor/config/VisitConfig.java
@@ -2,7 +2,9 @@ package com.macaku.user.interceptor.config;
 
 import com.macaku.user.interceptor.UserLoginInterceptor;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,27 +16,31 @@ public class VisitConfig implements WebMvcConfigurer {
 
     private final UserLoginInterceptor userLoginInterceptor;
 
+    @Value("${visit.swagger}")
+    private Boolean swaggerCanBeVisited;
+
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
-            registry.addInterceptor(userLoginInterceptor)
-                    .addPathPatterns("/**")
-                    // 不拦截的路径
-                    .excludePathPatterns("/user/login")
-                    // 静态资源
-                    .excludePathPatterns("/media/**")
-                    // 接口文档
-                    // todo: 正式上线关闭接口文档
-                    .excludePathPatterns("/doc.html/**")
-                    .excludePathPatterns("/v3/api-docs/**")
-                    .excludePathPatterns("/webjars/**")
-                    .excludePathPatterns("/error")
-                    .excludePathPatterns("/favicon.ico")
-                    .excludePathPatterns("/swagger-resources/**")
-                    .excludePathPatterns("/swagger-ui/**")
-
-            ;
-
-
+        // 接口文档
+        String[] swaggers = {
+                "/doc.html/**",
+                "/v3/api-docs/**",
+                "/webjars/**",
+                "/error",
+                "/favicon.ico",
+                "/swagger-resources/**",
+                "/swagger-ui/**"
+        };
+        InterceptorRegistration registration =
+                registry.addInterceptor(userLoginInterceptor)
+                        .addPathPatterns("/**")
+                        // 不拦截的路径
+                        .excludePathPatterns("/user/login")
+                        // 静态资源
+                        .excludePathPatterns("/media/**");
+        if(Boolean.TRUE.equals(swaggerCanBeVisited)) {
+            registration.excludePathPatterns(swaggers);
+        }
     }
 
 
