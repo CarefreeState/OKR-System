60332da4f29ee13d314251e5e29abeaf568c1829
s:103 <mamingsheng103@yeah.net>
2024-03-11 00:14:47 +0800

    调整代码。

--- center-service/src/main/java/com/macaku/center/controller/user/UserController.java
+++ center-service/src/main/java/com/macaku/center/controller/user/UserController.java
@@ -80,7 +80,7 @@ public class UserController {
         return SystemJsonResponse.SYSTEM_SUCCESS();
     }
 
-    @PostMapping("/check/wx")
+    @GetMapping("/check/wx")
     @ApiOperation("验证微信用户")
     public SystemJsonResponse<String> wxIdentifyCheck() {
         Long userId = UserRecordUtil.getUserRecord().getId();
