28279cdb2c302b36efc9e871ddfc0d4f5d98fb0a
s:103 <mamingsheng103@yeah.net>
2024-03-11 20:11:06 +0800

    调整代码。

--- user-service/src/main/java/com/macaku/user/domain/dto/detail/LoginUser.java
+++ user-service/src/main/java/com/macaku/user/domain/dto/detail/LoginUser.java
@@ -45,7 +45,7 @@ public class LoginUser implements UserDetails {
 
     @Override
     public String getUsername() {
-        return user.getNickname();
+        return null;
     }
 
     @Override
