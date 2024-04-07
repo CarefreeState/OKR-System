b36bfe52d48fa9b2f7289b0b9436adf653fe5615
s:103 <2040484356@qq.com>
2024-04-08 01:20:29 +0800

    调整

---
+++ center-service/src/main/java/com/macaku/center/controller/medal/UserMedalController.java
@@ -0,0 +1,60 @@
+package com.macaku.center.controller.medal;
+
+import com.macaku.common.response.SystemJsonResponse;
+import com.macaku.medal.domain.vo.UserMedalVO;
+import com.macaku.medal.service.UserMedalService;
+import com.macaku.user.util.UserRecordUtil;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created With Intellij IDEA
+ * Description:
+ * User: 马拉圈
+ * Date: 2024-04-08
+ * Time: 0:32
+ */
+@RestController
+@Slf4j
+@Api(tags = "用户勋章测试接口")
+@RequestMapping("/medal")
+@RequiredArgsConstructor
+public class UserMedalController {
+
+    private final UserMedalService userMedalService;
+
+    @GetMapping("/list/all")
+    @ApiOperation("获得用户的所有勋章")
+    public SystemJsonResponse<List<UserMedalVO>> getAll() {
+        Long userId = UserRecordUtil.getUserRecord().getId();
+        List<UserMedalVO> result = userMedalService.getUserMedalListAll(userId);
+        log.info("查询用户 {} 的所有勋章 : {} 个", userId, result.size());
+        return SystemJsonResponse.SYSTEM_SUCCESS(result);
+    }
+
+    @GetMapping("/list/unread")
+    @ApiOperation("获得用户的所有未读勋章")
+    public SystemJsonResponse<List<UserMedalVO>> getUnread() {
+        Long userId = UserRecordUtil.getUserRecord().getId();
+        List<UserMedalVO> result = userMedalService.getUserMedalListUnread(userId);
+        log.info("查询用户 {} 的所有未读勋章 : {} 个", userId, result.size());
+        return SystemJsonResponse.SYSTEM_SUCCESS(result);
+    }
+
+    @PostMapping("/read/{medalId}")
+    @ApiOperation("获得用户的所有勋章")
+    public SystemJsonResponse readUserMedal(@PathVariable("medalId") @NonNull @ApiParam("勋章 ID") Long medalId) {
+        Long userId = UserRecordUtil.getUserRecord().getId();
+        log.info("用户 {} 查看勋章 {}", userId, medalId);
+        userMedalService.readUserMedal(userId, medalId);
+        return SystemJsonResponse.SYSTEM_SUCCESS();
+    }
+
+}
---
+++ medal-service/src/main/java/com/macaku/medal/domain/vo/UserMedalVO.java
@@ -0,0 +1,37 @@
+package com.macaku.medal.domain.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+
+import java.util.Date;
+
+/**
+ * Created With Intellij IDEA
+ * Description:
+ * User: 马拉圈
+ * Date: 2024-04-08
+ * Time: 0:40
+ */
+@ApiModel("用户勋章")
+@Data
+public class UserMedalVO {
+
+    @ApiModelProperty("勋章名称")
+    private String name;
+
+    @ApiModelProperty("勋章描述")
+    private String description;
+
+    @ApiModelProperty("勋章 URL")
+    private String url;
+
+    @ApiModelProperty("勋章等级")
+    private Integer level;
+
+    @ApiModelProperty("勋章是否已读")
+    private Boolean isRead;
+
+    @ApiModelProperty("勋章颁布时间")
+    private Date issueTime;
+}
