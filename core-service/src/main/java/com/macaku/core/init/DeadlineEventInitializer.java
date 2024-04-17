529c80dc5d447d9a28b0f66d706c2de017b30dba
s:103 <2040484356@qq.com>
2024-04-18 01:27:47 +0800

    调整

--- core-service/src/main/java/com/macaku/core/init/DeadlineEventInitializer.java
+++ core-service/src/main/java/com/macaku/core/init/DeadlineEventInitializer.java
@@ -8,6 +8,8 @@ import com.macaku.core.init.handler.ext.SecondQuadrantEventHandler;
 import com.macaku.core.init.handler.ext.ThirdQuadrantEventHandler;
 import com.macaku.core.init.util.QuadrantDeadlineUtil;
 import com.macaku.core.mapper.OkrCoreMapper;
+import com.macaku.xxljob.annotation.XxlRegister;
+import com.xxl.job.core.handler.annotation.XxlJob;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
@@ -21,6 +23,14 @@ import java.util.List;
 @RequiredArgsConstructor
 @Slf4j
 public class DeadlineEventInitializer implements ApplicationListener<ApplicationStartedEvent> {
+
+    private final static String AUTHOR = "macaku";
+
+    private final static String ROUTE = "ROUND";
+
+    private final static int TRIGGER_STATUS = 0;
+
+    private final static String CRON = "59 23 23 ? * 1 *";
 
     private final OkrCoreMapper okrCoreMapper;
 
@@ -47,14 +57,25 @@ public class DeadlineEventInitializer implements ApplicationListener<Application
         handlerChain.handle(deadlineEvent, nowTimestamp);
     }
 
-    @Override
-    public void onApplicationEvent(ApplicationStartedEvent event) {
-        log.warn("--> --> --> 应用启动成功 --> 开始恢复定时任务 --> --> -->");
+    private void action() {
         QuadrantDeadlineUtil.clear();
         // 获取定时任务
         List<DeadlineEvent> deadlineEvents = okrCoreMapper.getDeadlineEvents();
         // 处理定时任务
         CPUThreadPool.operateBatch(deadlineEvents, this::handleEvent);
+    }
+
+    @XxlJob(value = "initDeadlineJob")
+    @XxlRegister(cron = CRON, executorRouteStrategy = ROUTE,
+            author = AUTHOR,  triggerStatus = TRIGGER_STATUS, jobDesc = "【固定任务】刷新截止时间的任务")
+    public void initDeadlineJob() {
+        action();
+    }
+
+    @Override
+    public void onApplicationEvent(ApplicationStartedEvent event) {
+        log.warn("--> --> --> 应用启动成功 --> 开始恢复定时任务 --> --> -->");
+        action();
         log.warn("<-- <-- <-- <-- <-- 定时任务恢复成功 <-- <-- <-- <-- <--");
     }
 }
