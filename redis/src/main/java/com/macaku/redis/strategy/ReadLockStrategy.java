0dc91d30458cfe4d1313064425f9c01811e06fa1
s:103 <2040484356@qq.com>
2024-09-05 16:05:11 +0800

    提交 redis 代码

---
+++ redis/src/main/java/com/macaku/redis/strategy/ReadLockStrategy.java
@@ -0,0 +1,28 @@
+package com.macaku.redis.strategy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created With Intellij IDEA
+ * Description:
+ * User: 马拉圈
+ * Date: 2024-07-11
+ * Time: 23:31
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class ReadLockStrategy implements LockStrategy {
+
+    private final RedissonClient redisClient;
+
+    @Override
+    public RLock apply(String key) {
+        log.info("读锁 RLock {}", key);
+        return redisClient.getReadWriteLock(key).readLock();
+    }
+}
---
+++ redis/src/main/java/com/macaku/redis/strategy/SimpleLockStrategy.java
@@ -0,0 +1,28 @@
+package com.macaku.redis.strategy;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created With Intellij IDEA
+ * Description:
+ * User: 马拉圈
+ * Date: 2024-07-11
+ * Time: 23:25
+ */
+@Component
+@Slf4j
+@RequiredArgsConstructor
+public class SimpleLockStrategy implements LockStrategy {
+
+    private final RedissonClient redisClient;
+
+    @Override
+    public RLock apply(String key) {
+        log.info("普通锁 RLock {}", key);
+        return redisClient.getLock(key);
+    }
+}
