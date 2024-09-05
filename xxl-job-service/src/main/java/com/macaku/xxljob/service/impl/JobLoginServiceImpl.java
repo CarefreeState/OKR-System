04926ee107f1437305703689df44a0aa58745625
s:103 <2040484356@qq.com>
2024-09-05 15:12:05 +0800

    调整 xxljob 代码

--- xxl-job-service/src/main/java/com/macaku/xxljob/service/impl/JobLoginServiceImpl.java
+++ xxl-job-service/src/main/java/com/macaku/xxljob/service/impl/JobLoginServiceImpl.java
@@ -1,7 +1,6 @@
 package com.macaku.xxljob.service.impl;
 
 import cn.hutool.http.HttpRequest;
-import cn.hutool.http.HttpResponse;
 import com.macaku.common.exception.GlobalServiceException;
 import com.macaku.xxljob.config.Admin;
 import com.macaku.xxljob.config.XxlUrl;
@@ -10,8 +9,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.net.HttpCookie;
-import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
@@ -25,18 +22,18 @@ public class JobLoginServiceImpl implements JobLoginService {
 
     @Override
     public String login() {
-        String url = admin.getAddresses() + xxlUrl.getLogin();
-        HttpResponse response = HttpRequest.post(url)
+        return HttpRequest.post(admin.getAddresses() + xxlUrl.getLogin())
                 .form("userName", admin.getUsername())
                 .form("password", admin.getPassword())
-                .execute();
-        List<HttpCookie> cookies = response.getCookies();
-        Optional<HttpCookie> cookieOpt = cookies.stream()
-                .filter(cookie -> cookie.getName().equals(XXL_JOB_LOGIN_IDENTITY)).findFirst();
-        return cookieOpt.map(HttpCookie::getValue)
+                .execute()
+                .getCookies()
+                .stream()
+                .filter(cookie -> cookie.getName().equals(XXL_JOB_LOGIN_IDENTITY))
+                .findFirst()
+                .map(HttpCookie::getValue)
                 .map(cookie -> String.format("%s=%s", XXL_JOB_LOGIN_IDENTITY, cookie))
                 .orElseThrow(() ->
-                    new GlobalServiceException("get xxl-job cookie error!")
+                        new GlobalServiceException("get xxl-job cookie error!")
                 );
     }
 
